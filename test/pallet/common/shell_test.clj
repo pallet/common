493e8186c414eb3b5b554bae9d449deae4705a4a
Hugo Duncan <hugo@hugoduncan.org>
2011-04-21 11:45:32 -0400

    Add test for common/shell

---
+++ test/pallet/common/shell_test.clj
@@ -0,0 +1,18 @@
+(ns pallet.common.shell-test
+  (:use
+   pallet.common.shell
+   clojure.test)
+  (:require
+   [clojure.contrib.logging :as logging]))
+
+(deftest system-test
+  (cond
+   (.canRead (java.io.File. "/usr/bin/true")) (is (= {:exit 0 :out "" :err ""}
+                                                     (system "/usr/bin/true")))
+   (.canRead (java.io.File. "/bin/true")) (is (= {:exit 0 :out "" :err ""}
+                                                 (system "/bin/true")))
+   :else (logging/warn "Skipping system-test")))
+
+(deftest bash-test
+  (is (= {:exit 0 :out "fred\n" :err ""}
+         (bash "echo fred"))))
