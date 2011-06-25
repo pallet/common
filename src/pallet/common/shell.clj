04be2f26864dbd253ca035bb8b4443e013cc27fc
Hugo Duncan <hugo@hugoduncan.org>
2011-06-25 00:59:57 -0400

    Remove use of clojure.contrib

--- src/pallet/common/shell.clj
+++ src/pallet/common/shell.clj
@@ -2,14 +2,14 @@
   "Shell functions"
   (:require
    [pallet.common.filesystem :as filesystem]
-   [clojure.contrib.shell :as shell]))
+   [clojure.java.shell :as shell]))
 
 (defn system
   "Launch a system process, return a map containing the exit code, standard
   output and standard error of the process."
   [cmd]
-  (apply shell/sh :return-map true (.split cmd " ")))
+  (apply shell/sh (.split cmd " ")))
 
 (defn bash [cmds]
   (filesystem/with-temp-file [file cmds]
-    (shell/sh :return-map true "/usr/bin/env" "bash"  (.getPath file))))
+    (shell/sh "/usr/bin/env" "bash" (.getPath file))))
