5da8f5a49286cc451a9100339f3ee51a4a0edcbf
Hugo Duncan <hugo@hugoduncan.org>
2011-09-24 10:05:48 -0400

    Add try-context, with-context, with-logged-context and throw+
    Remove atom from common.context, and made on-enter and on-exit callbacks composed
    with juxt.

--- src/pallet/common/context.clj
+++ src/pallet/common/context.clj
@@ -1,39 +1,132 @@
 (ns pallet.common.context
   "A hierarchical context, with callbacks on entry and exit of a context."
-  (:refer-clojure :exclude [make-hierarchy]))
+  (:refer-clojure :exclude [make-hierarchy])
+  (:require
+   [clojure.string :as string]
+   [clojure.tools.logging :as logging]
+   [slingshot.core :as slingshot]))
+
+(def ^{:dynamic true :doc "Thread specific current context"}
+  *current-hierarchy*)
 
 (defn make-hierarchy
   "Returns a new context hierarchy. Accepts optional callbacks for :on-enter
    and on-exit, which are called for every change in context."
-  [& {:keys [on-enter on-exit]
-      :or {on-enter (constantly nil) on-exit (constantly nil)}}]
-  (atom {::on-enter on-enter ::on-exit on-exit}))
+  [& {:keys [on-enter on-exit] :as options}]
+  options)
 
-(def ^{:private true :doc "The default hierarchy"}
-  default-hierarchy (make-hierarchy))
-
-(defn options!
+(defn options
   "Set the options for a hierarchy. Accepts :on-enter and :on-exit callback
    functions."
   ([hierarchy {:keys [on-enter on-exit] :as options}]
-     (swap! hierarchy merge {::on-enter (or on-enter (::on-enter hierarchy))
-                             ::on-exit (or on-exit (::on-exit hierarchy))}))
+     (merge
+      hierarchy
+      (merge-with juxt (select-keys hierarchy [:on-enter :on-exit]) options)))
   ([{:keys [on-enter on-exit] :as options}]
-     (options! default-hierarchy options)))
+     (options *current-hierarchy* options)))
+
+(defn push-context
+  [hierarchy key context]
+  (if context
+    (update-in hierarchy [key] (fn [v] (conj (or v []) context)))
+    hierarchy))
+
+(defn on-enter
+  [hierarchy key context]
+  (when-let [f (:on-enter hierarchy)]
+    (f hierarchy key context)))
+
+(defn on-exit
+  [hierarchy key context]
+  (when-let [f (:on-exit hierarchy)]
+    (f hierarchy key context)))
+
+(defn current-context
+  "Return the current context."
+  ([hierarchy key] (get hierarchy key))
+  ([hierarchy] (current-context hierarchy ::default))
+  ([] (current-context *current-hierarchy*)))
+
+(defn- options-and-body
+  "Extract an optional options map from the first component of body."
+  [body]
+  (let [options (if (map? (first body)) (first body) {})
+        body (if (map? (first body)) (rest body) body)]
+    [options body]))
 
 (defmacro in-context
   "Create a scope by pushing a context onto the context hierarchy. On exit of
    the body, the context is popped."
-  [context [& {:keys [hierarchy key]
-               :or {hierarchy `(deref (var default-hierarchy))
-                    key ::default}}] & body]
-  `(let [context# ~context
-         hierarchy# ~hierarchy
-         key# ~key]
-     (swap! hierarchy# update-in [key#] (fn [v#] (conj (or v# []) context#)))
-     (try
-       ((::on-enter @hierarchy#) hierarchy# key# context#)
-       ~@body
-       (finally
-        (swap! hierarchy# update-in [key#] pop)
-        ((::on-exit @hierarchy#) hierarchy# key# context#)))))
+  [context & body]
+  (let [[options body] (options-and-body body)
+        {:keys [hierarchy key on-enter on-exit]
+         :or {hierarchy `(if (bound? #'*current-hierarchy*)
+                           *current-hierarchy*
+                           (make-hierarchy))
+              key ::default}} options]
+    `(let [context# ~context
+           hierarchy# ~hierarchy
+           key# ~key
+           options# ~(select-keys options [:on-enter :on-exit])]
+       (binding [*current-hierarchy* (options
+                                      (push-context hierarchy# key# context#)
+                                      options#)]
+         (try
+           (on-enter *current-hierarchy* key# context#)
+           ~@body
+           (finally
+            (on-exit *current-hierarchy* key# context#)))))))
+
+(defmacro try-context
+  "Execute body, wrapping any exceptions in an exception which includes the
+   current context."
+  [& body]
+  (let [[options body] (options-and-body body)
+        {:keys [exception-type hierarchy key]
+         :or {exception-type :runtime-exception
+              hierarchy `*current-hierarchy*
+              key ::default}} options]
+    `(slingshot/try+
+      ~@body
+      (catch Exception e#
+        (slingshot/throw+
+         {:type ~exception-type
+          :context (current-context ~hierarchy ~key)
+          :message (.getMessage e#)})))))
+
+(defmacro with-context
+  "Wraps the body with a context, and re-throws wrapped exceptions"
+  [context & body]
+  (let [[options body] (options-and-body body)]
+    `(let [options# ~options]
+       (in-context ~context options#
+        (try-context options#
+         ~@body)))))
+
+(defn throw+
+  "Throws a map, containing the current context on the :context key"
+  [& {:as exception-map}]
+  (slingshot/throw+
+   (assoc exception-map
+     :context (dissoc *current-hierarchy* :on-enter :on-exit))))
+
+(defn context-as-string
+  [context]
+  (string/join " " context))
+
+(defmacro with-logged-context
+  "Log context entries and exits"
+  [& body]
+  `(in-context
+    nil
+    {:on-enter (fn [hierarchy# key# context#]
+                 (when context#
+                   (logging/infof
+                    "-> %s"
+                    (context-as-string (current-context hierarchy# key#)))))
+     :on-exit (fn [hierarchy# key# context#]
+                (when context#
+                  (logging/infof
+                   "<- %s"
+                   (context-as-string (current-context hierarchy# key#)))))}
+    ~@body))
--- test/pallet/common/context_test.clj
+++ test/pallet/common/context_test.clj
@@ -1,75 +1,83 @@
 (ns pallet.common.context-test
   (:require
-   [pallet.common.context :as context])
+   [pallet.common.context :as context]
+   [pallet.common.logging.logutils :as logutils]
+   [slingshot.core :as slingshot])
   (:use
    clojure.test))
 
 (deftest in-context-test
   (testing "single level"
     (context/in-context
-     "fred" []
-     (is (= ["fred"]
-              (:pallet.common.context/default
-                @@#'pallet.common.context/default-hierarchy))))
-    (is (= []
-             (:pallet.common.context/default
-               @@#'pallet.common.context/default-hierarchy))))
+     "fred"
+     (is (= ["fred"] (context/current-context))))
+    (is (not (bound? #'context/*current-hierarchy*))))
   (testing "nested levels"
     (context/in-context
-     "fred" []
-     (is (= ["fred"]
-              (:pallet.common.context/default
-                @@#'pallet.common.context/default-hierarchy)))
+     "fred"
+     (is (= ["fred"] (context/current-context)))
      (context/in-context
-      "bloggs" []
-      (is (= ["fred" "bloggs"]
-               (:pallet.common.context/default
-                 @@#'pallet.common.context/default-hierarchy))))
-     (is (= ["fred"]
-              (:pallet.common.context/default
-                @@#'pallet.common.context/default-hierarchy))))
-    (is (= []
-             (:pallet.common.context/default
-               @@#'pallet.common.context/default-hierarchy))))
+      "bloggs"
+      (is (= ["fred" "bloggs"] (context/current-context))))
+     (is (= ["fred"] (context/current-context))))
+    (is (not (bound? #'context/*current-hierarchy*))))
   (testing "non-default key"
     (context/in-context
-     "fred" [:key :fred]
+     "fred" {:key :fred}
      (is (= ["fred"]
-              (:fred @@#'pallet.common.context/default-hierarchy))))
-    (is (= []
-             (:fred @@#'pallet.common.context/default-hierarchy))))
+              (context/current-context context/*current-hierarchy* :fred))))
+    (is (not (bound? #'context/*current-hierarchy*))))
   (testing "non-default hierarchy"
     (let [hierarchy (context/make-hierarchy)]
       (context/in-context
-       "fred" [:hierarchy hierarchy]
-       (is (= ["fred"] (:pallet.common.context/default @hierarchy))))
-      (is (= [] (:pallet.common.context/default @hierarchy))))))
+       "fred" {:hierarchy hierarchy}
+       (is (= ["fred"] (context/current-context))))
+      (is (nil? (context/current-context hierarchy))))))
 
 (deftest in-context-with-callback-test
   (let [seen-enter (atom nil)
-        seen-exit (atom nil)
-        hierarchy (context/make-hierarchy)]
-    (context/options!
-     hierarchy
-     {:on-enter (fn on-enter [hierarchy1 key value]
-                  (is (= hierarchy hierarchy1))
-                  (is (= key :pallet.common.context/default))
-                  (is (= value "fred"))
-                  (is (not @seen-enter))
-                  (is (not @seen-exit))
-                  (reset! seen-enter true))
-      :on-exit (fn on-exit [hierarchy1 key value]
-                  (is (= hierarchy hierarchy1))
-                  (is (= key :pallet.common.context/default))
-                  (is (= value "fred"))
-                  (is @seen-enter)
-                  (is (not @seen-exit))
-                  (reset! seen-exit true))})
+        seen-exit (atom nil)]
     (context/in-context
-     "fred" [:hierarchy hierarchy]
-     (is (= ["fred"] (:pallet.common.context/default @hierarchy)))
+     "fred" {:on-enter (fn on-enter [hierarchy key value]
+                         (is (= key :pallet.common.context/default))
+                         (is (= value "fred"))
+                         (is (not @seen-enter))
+                         (is (not @seen-exit))
+                         (reset! seen-enter true))
+             :on-exit (fn on-exit [hierarchy key value]
+                        (is (= key :pallet.common.context/default))
+                        (is (= value "fred"))
+                        (is @seen-enter)
+                        (is (not @seen-exit))
+                        (reset! seen-exit true))}
+     (is (= ["fred"] (context/current-context)))
      (is @seen-enter)
      (is (not @seen-exit)))
-    (is (= [] (:pallet.common.context/default @hierarchy)))
+    (is (not (bound? #'context/*current-hierarchy*)))
     (is @seen-enter)
     (is @seen-exit)))
+
+(deftest try-context-test
+  (slingshot/try+
+   (context/in-context
+    "fred"
+    (context/try-context
+     (throw (Exception. "msg"))))
+   (catch map? e
+     (is (= ["fred"] (:context e)))
+     (is (= "msg" (:message e))))))
+
+(deftest with-context-test
+  (slingshot/try+
+   (context/with-context "fred"
+     (throw (Exception. "msg")))
+   (catch map? e
+     (is (= ["fred"] (:context e)))
+     (is (= "msg" (:message e))))))
+
+(deftest with-logged-context-test
+  (is (= "info -> fred\ninfo <- fred\n"
+         (logutils/logging-to-string
+           (context/with-logged-context
+             (context/with-context "fred"
+               nil))))))
