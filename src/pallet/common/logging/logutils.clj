9d0178f6093f8c9791d56346a33200d0d7794eba
Hugo Duncan <hugo@hugoduncan.org>
2011-09-25 10:02:23 -0400

    Upgrade to tools.logging 0.2.0
    Introduce tools-logging-compat implementation macro to provide implementation
    details across 0.1.2 and 0.2.0

--- src/pallet/common/logging/logutils.clj
+++ src/pallet/common/logging/logutils.clj
@@ -11,65 +11,114 @@
     (require ns)
     (catch Exception _)))
 
-;;; A null logger
-;;; Suppresses all logging.  Can be useful to quiet test cases.
-(deftype NullLog
-    []
-  clojure.tools.logging.Log
-  (impl-enabled? [log level] false)
-  (impl-write! [log level throwable message]))
 
-(def null-log (delay (NullLog.)))
+(defmacro tools-logging-compat []
+  (if (try
+        (Class/forName "clojure.tools.logging.Log")
+        (catch ClassNotFoundException _))
+    `(do
+       ;; pre tools.logging 0.2.0
+       (deftype ~'NullLogger
+           []
+         clojure.tools.logging.Log
+         (impl-enabled? [log# level#] false)
+         (impl-write! [log# level# throwable# message#]))
+       (def null-log (delay (NullLogger.)))
 
-(deftype NullLogger
-    []
-  clojure.tools.logging.LogFactory
-  (impl-name [factory] "null logger")
-  (impl-get-log [factory log-ns] @null-log))
-
-(def null-logger (delay (NullLogger.)))
+       (deftype ~'NullLoggerFactory
+           []
+         clojure.tools.logging.LogFactory
+         (impl-name [factory#] "null logger")
+         (impl-get-log [factory# log-ns#] @null-log))
+       (def null-logger-factory (delay (NullLoggerFactory.)))
 
 ;;; A stdout logger
 ;;; Logs everyting to stdout.  Can be useful to test logging.
-(deftype StdoutLog
-    []
-  clojure.tools.logging.Log
-  (impl-enabled? [log level] true)
-  (impl-write! [log level throwable message]
-    (println (name level) message)
-    (when throwable
-      (stacktrace/print-stack-trace
-       (stacktrace/root-cause throwable)))))
+       (deftype ~'StdoutLogger
+           []
+         clojure.tools.logging.Log
+         (impl-enabled? [log# level#] true)
+         (impl-write! [log# level# throwable# message#]
+           (println (name level#) message#)
+           (when throwable#
+             (stacktrace/print-stack-trace
+              (stacktrace/root-cause throwable#)))))
+       (def stdout-log (delay (StdoutLogger.)))
 
-(def stdout-log (delay (StdoutLog.)))
+       (deftype ~'StdoutLoggerFactory
+           []
+         clojure.tools.logging.LogFactory
+         (impl-name [factory#] "stdout logger")
+         (impl-get-log [factory# log-ns#] @stdout-log))
+       (def stdout-logger-factory (delay (StdoutLoggerFactory.)))
 
-(deftype StdoutLogger
-    []
-  clojure.tools.logging.LogFactory
-  (impl-name [factory] "stdout logger")
-  (impl-get-log [factory log-ns] @stdout-log))
+       (defmacro with-logger-factory
+         [factory# & body#]
+         `(binding [logging/*log-factory* ~factory#] ~@body#)))
+    `(do
+       ;; tools.logging 0.2.0 and up
+;;; A null logger
+;;; Suppresses all logging.  Can be useful to quiet test cases.
+       (deftype ~'NullLogger
+           []
+         clojure.tools.logging.impl.Logger
+         (enabled? [log# level#] false)
+         (write! [log# level# throwable# message#]))
 
-(def stdout-logger (delay (StdoutLogger.)))
+       (def null-log (delay (NullLogger.)))
+
+       (deftype ~'NullLoggerFactory
+           []
+         clojure.tools.logging.impl.LoggerFactory
+         (name [factory#] "null logger")
+         (get-logger [factory# log-ns#] @null-log))
+       (def null-logger-factory (delay (NullLoggerFactory.)))
+
+;;; A stdout logger
+;;; Logs everyting to stdout.  Can be useful to test logging.
+       (deftype ~'StdoutLogger
+           []
+         clojure.tools.logging.impl.Logger
+         (enabled? [log# level#] true)
+         (write! [log# level# throwable# message#]
+           (println (name level#) message#)
+           (when throwable#
+             (stacktrace/print-stack-trace
+              (stacktrace/root-cause throwable#)))))
+       (def stdout-log (delay (StdoutLogger.)))
+
+       (deftype ~'StdoutLoggerFactory
+           []
+         clojure.tools.logging.impl.LoggerFactory
+         (name [factory#] "stdout logger")
+         (get-logger [factory# log-ns#] @stdout-log))
+       (def stdout-logger-factory (delay (StdoutLoggerFactory.)))
+
+       (defmacro with-logger-factory
+         [factory# & body#]
+         `(binding [logging/*logger-factory* ~factory#] ~@body#)))))
+
+(tools-logging-compat)
 
 
 ;;; Macros to use specific logging implementations in a given scope
 (defmacro logging-to-stdout
   "Send log messages to stdout for inspection"
   [& forms]
-  `(binding [clojure.tools.logging/*log-factory* @stdout-logger]
+  `(with-logger-factory @stdout-logger-factory
      ~@forms))
 
 (defmacro logging-to-string
   "Send log messages to a string for inspection"
   [& forms]
   `(with-out-str
-     (binding [clojure.tools.logging/*log-factory* @stdout-logger]
+     (with-logger-factory @stdout-logger-factory
        ~@forms)))
 
 (defmacro suppress-logging
   "Prevent log messages to reduce test log noise"
   [& forms]
-  `(binding [clojure.tools.logging/*log-factory* @null-logger]
+  `(with-logger-factory @null-logger-factory
      ~@forms))
 
 (defmacro with-context
