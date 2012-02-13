185e23962fc5e1e93b01e38c29f5b0d91bad6f20
Hugo Duncan <hugo@hugoduncan.org>
2012-02-13 11:36:49 -0500

    Extend slingshot testing utilities
    Adds is-thrown-slingshot?, is-thrown-with-msg-slingshot?, ex-data and slingshot-object

--- src/pallet/common/slingshot_test_util.clj
+++ src/pallet/common/slingshot_test_util.clj
@@ -1,7 +1,7 @@
 (ns pallet.common.slingshot-test-util
   (:use clojure.test))
 
-(defn exception-class
+(defn slingshot-exception-class
   "Return the best guess at a slingshot exception class."
   []
   (try
@@ -16,4 +16,24 @@
 (defmacro is-thrown-slingshot?
   "clojure.test clause for testing that a slingshot exception is thrown."
   [& body]
-  `(is (~'thrown? ~(exception-class) ~@body)))
+  `(is (~'thrown? ~(slingshot-exception-class) ~@body)))
+
+(defmacro is-thrown-with-msg-slingshot?
+  "clojure.test clause for testing that a slingshot exception is thrown."
+  [& body]
+  `(is (~'thrown-with-msg? ~(slingshot-exception-class) ~@body)))
+
+(def ex-data
+  (or
+   (try
+     (require 'slingshot.support)
+     (ns-resolve 'slingshot.support 'ex-data)
+     (catch Exception _))
+   (fn ex-data [e] (.data e))))
+
+(defn slingshot-object
+  "Return the object thrown using slingshot."
+  [e]
+  (let [data (ex-data e)]
+    (or (:object data)
+        (:obj data))))
