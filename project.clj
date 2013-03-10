9d2c1accf4cdd01c4aca86c1746ceb5b082ec1fe
Hugo Duncan <hugo@hugoduncan.org>
2013-03-10 19:35:22 -0400

    Use lein as the build tool

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.palletops/pallet-common "0.4.0-SNAPSHOT"
+  :description "Common functions used across pallet projects"
+  :url "http://palletops.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.0"]])
